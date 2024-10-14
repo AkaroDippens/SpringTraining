281e96fcf94b33b0ab113d516d299279eb240a2b
AkaroDippens <mirzoev.nidzhat@mail.ru>
2024-10-14 19:44:03 +0300

    Getting started

---
+++ src/main/kotlin/com/example/uchebpracticaspring/UchebPracticaSpringApplication.kt
@@ -0,0 +1,11 @@
+package com.example.uchebpracticaspring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UchebPracticaSpringApplication
+
+fun main(args: Array<String>) {
+    runApplication<UchebPracticaSpringApplication>(*args)
+}
